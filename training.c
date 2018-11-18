b5122b34f7c0c583e9150ab4b9e991f64d32eb0a
jangseokgyu <jsk053@hanyang.ac.kr>
2018-11-18 22:06:15 +0900

    ver.2

--- training.c
+++ training.c
@@ -1,5 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
 
 typedef struct Node {
 	char character;
@@ -7,6 +12,34 @@ typedef struct Node {
 }Node;
 
 int main() {
+
+	Node* head = NULL;
+	Node* tail = NULL;
+	char temp=NULL; 
+
+	printf("write num\n");
+
+	while (temp != '\n') {
+		Node* newnode = (Node*)malloc(sizeof(Node));
+		temp = getchar();
+		newnode->character = temp;
+		newnode->next = NULL;
+
+		if (head == NULL)
+			head = newnode;
+		else
+			tail->next = newnode;
+
+		tail = newnode;
+	}
+	Node* temp1 = (Node*)malloc(sizeof(Node));
+	temp1 = head;
+
+	while (temp1 != tail) {
+		printf("[%c]", temp1->character);
+		temp1 = temp1->next;q
+	}
+
 	int i;
 	printf("write num\n");
 	scanf("%d", &i);
